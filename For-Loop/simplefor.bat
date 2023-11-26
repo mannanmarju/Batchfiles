16fc11b9405be8abed12715e5b4f85d20e87746a
mannanmarju <150770587+mannanmarju@users.noreply.github.com>
2023-11-25 19:36:01 -0800

    adding simple for loop

---
+++ For-Loop/simplefor.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM this is an example of for loop
+REM for loop consists of three things START, STEP, END
+REM Start is what to start with? when and how it should START.
+REM STEP is how many steps it should do either add or decrease
+REM END when it should end
+REM l means list of numbers
+
+for /l %%i in (1,1,5) do echo hello
